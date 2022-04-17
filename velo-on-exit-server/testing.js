7b8784b744e445cb6b87b3b0f9a4a944d733c4ae
Nathan Chesmar <nathan.chesmar@gmail.com>
2022-04-17 11:52:39 -0700

    untrack files in .gitignore

--- velo-on-exit-server/testing.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-playerObj = JSON.parse(
-  fs.readFileSync(
-    '../grab-dat-data/scraped_statcast_data/20220414-albert-pujols-405395.json'
-  )
-)
-console.log(Object.keys(playerObj[0]))
-
-// statcast games have this "Game Date": "2022-04-13"
